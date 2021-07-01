b9ca9ed20d6c4ff3b96d11185c98319b9440a58a
Raivis80 <77964146+Raivis80@users.noreply.github.com>
2021-07-01 18:49:58 +0100

    added added css styling, fixed target position js code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,66 +1,64 @@
 
-    let width;
-    let height;
-
-    let numWidth;
-    let numHeight;
-
-    let positionx; 
-    let positiony;
 
     
-// get div elements 
-let newDiv = document.getElementsByTagName('div');
+let gameWindow = document.getElementById('gameWindow');
+
+let width;
+let height;
+
+let numWidth;
+let numHeight;
+
+let positionx; 
+let positiony;
 
 //Game Window-And target-variables
-let gameWindow = newDiv[0];
-let div1 = newDiv[1];
-let div2 = newDiv[2];
-let div3 = newDiv[3];
+let target1 = gameWindow.children[0];
+let target2 = gameWindow.children[1];
+let target3 = gameWindow.children[2];
 
 // -------------------RANDOM NUMBER GENERATOR------------------
 // getting width and height numbers based on the screen size.
-function getPosition() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-}
 
 // Passing width and height numbers into a random number generator to
 // get random x and y coardinates for the game objects.
 function positionYX() {
+    width = gameWindow.offsetWidth;
+    height = gameWindow.offsetHeight;
+
     numWidth = Math.floor(Math.random() * (width - 50));
     numHeight = Math.floor(Math.random() * (height - 50)); 
     
     positionx =  numWidth + 'px';
     positiony = numHeight + 'px';
-}
+    console.log(positionx, positiony)
+};
+
+positionYX()
 
 // Get random number and position in one function.
-function getRandom() {
-    getPosition();    
-    positionYX();
-}
 
 // create random Object for individual targets------------
 function randomObject1() {
-    getRandom()  
-    div1.style.left = positionx;
-    div1.style.top = positiony;
+    positionYX()
+    target1.style.left = positionx;
+    target1.style.top = positiony;
+    
 }
 
 function randomObject2() {
-    getRandom()  
-    div2.style.left = positionx;
-    div2.style.top = positiony;
+    positionYX() 
+    target2.style.left = positionx;
+    target2.style.top = positiony;
 }
 
 function randomObject3() {
-    getRandom()  
-    div3.style.left = positionx;
-    div3.style.top = positiony;
+    positionYX()
+    target3.style.left = positionx;
+    target3.style.top = positiony;
 }
 
-// Target Objects for each game level
+// Game levels
 function gameOne() {
     randomObject1();
 }
@@ -91,9 +89,9 @@ function reset() {
 // Score counter-------------------------------------------------
 
 // Target event listermers
-div1.addEventListener('click', randomObject1);
-div2.addEventListener('click', randomObject2);
-div3.addEventListener('click', randomObject3);
+target1.addEventListener('click', randomObject1);
+target2.addEventListener('click', randomObject2);
+target3.addEventListener('click', randomObject3);
 
 // game window event listener
 gameWindow.addEventListener('click', function() {
@@ -102,7 +100,7 @@ gameWindow.addEventListener('click', function() {
 });
 
 
-// Game Level sellection------------------------------------------
+// Game Setup based on Level sellection-----------------------
 
 
 const getP = document.getElementsByTagName('p');
